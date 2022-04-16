5e57435b9c3c5e0796798f38bcb1aff78db03afd
MAYUR JITUBHAI GHEVARIYA <50697840+mrghevariya106@users.noreply.github.com>
2022-04-16 15:52:04 +0530

    added login api

--- central-perk-cafe-backend/routes/user.js
+++ central-perk-cafe-backend/routes/user.js
@@ -3,6 +3,9 @@ const { route } = require("..");
 const connection = require("../connection");
 const router = express.Router();
 
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+
 router.post("/signup", (req, res) => {
   let user = req.body;
   query = "select email,password,role,status from user where email=?";
@@ -19,6 +22,30 @@ router.post("/signup", (req, res) => {
         });
       } else {
         return res.status(400).json({ message: "Email Already in Use or Exists" });
+      }
+    } else {
+      return res.status(500).json(err);
+    }
+  });
+});
+
+router.post("/login", (req, res) => {
+  const user = req.body;
+  query = "select email, password, role, status from user where email=?";
+  connection.query(query, [user.email], (err, results) => {
+    if (!err) {
+      if (results.length <= 0 || results[0].password != user.password) {
+        return res.status(401).json({ message: "Incorrect Username or Password" });
+      } else if (results[0].status === "false") {
+        return res.status(401).json({ message: "Wait for Admin Approval" });
+      } else if (results[0].password == user.password) {
+        const response = { email: results[0].email, role: results[0].role };
+        const accessToken = jwt.sign(response, process.env.ACCESS_TOKEN, {
+          expiresIn: "8h",
+        });
+        res.status(200).json({ token: accessToken });
+      } else {
+        res.status(400).json({ message: "Something went wrong. Please try again." });
       }
     } else {
       return res.status(500).json(err);
