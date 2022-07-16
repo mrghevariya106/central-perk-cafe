83b47f3bf58d0ad3d70f9ee432e497760d629bcf
MAYUR JITUBHAI GHEVARIYA <50697840+mrghevariya106@users.noreply.github.com>
2022-07-16 11:25:15 +0530

    added bill api changes

--- central-perk-cafe-backend/routes/bill.js
+++ central-perk-cafe-backend/routes/bill.js
@@ -43,4 +43,64 @@ router.post('/generateReport', auth.authenticateToken, (req, res) => {
     });
 });
 
+router.post('/getPdf', auth.authenticateToken, function(req, res) {
+    const orderDetails = req.body;
+    const pdfPath = './generated_pdf/' + orderDetails.uuid + '.pdf';
+    if(fs.existsSync(pdfPath)) {
+        res.contentType("application/pdf");
+        fs.createReadStream(pdfPath).pipe(res);
+    } else {
+        var productDetailsReport = JSON.parse(orderDetails.productDetails);
+        ejs.renderFile(path.join(__dirname,'',"report.ejs"),{
+            productDetails: productDetailsReport,
+            name: orderDetails.name,
+            email: orderDetails.email,
+            contactNumber: orderDetails.contactNumber,
+            paymentMethod: orderDetails.paymentMethod,
+            totalAmount: orderDetails.totalAmount,
+        }, (err, data) => {
+            if (err) {
+                return res.status(500).json(err);
+            } else {
+                pdf.create(data).toFile('./generated_pdf/' + orderDetails.uuid + ".pdf", function(err, result) {
+                    if (err) {
+                        console.log(err);
+                        return res.status(500).json(err);
+                    } else {
+                        res.contentType("application/pdf");
+                        fs.createReadStream(pdfPath).pipe(res);
+                    }
+                });
+            }
+        });
+    }
+});
+
+router.get('/getBills', auth.authenticateToken, (req,res,next) => {
+    var query = "select *from bill order by id DESC";
+    connection.query(query,(err, results) => {
+        if(!err) {
+            return res.status(200).json(results);
+        } else {
+            return res.status(500).json(err);
+        }
+    });
+});
+
+router.delete('/delete/:id', auth.authenticateToken, (req,res,next) => {
+    const id = req.params.id;
+    var query = "delete from bill where id=?";
+    connection.query(query, [id], (err, results) => {
+        if(!err) {
+            if(results.affectedRows == 0) {
+                return res.status(404).json({ message: "Bill id does not found"});
+            } else {
+                return res.status(200).json({ message: "Bill id deleted successfully"});
+            }
+        } else {
+            return res.status(500).json(err);
+        }
+    });
+});
+
 module.exports = router;
